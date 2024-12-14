42c71ae5e7ce74cddc238011e024dbc7fb388ae7
quincyqqe <0972900039@mail.ru>
2024-12-14 01:10:48 +0100

    add font & start styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-	return <div className="text-3xl bold justify-center flex">Test</div>
+	return (
+		<div className=''>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores maiores
+			provident ea incidunt earum minus cumque deleniti a, consequuntur aperiam
+			voluptatum repellat placeat consectetur laudantium quidem? Amet nesciunt
+			dolore earum!
+		</div>
+	)
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'blue-primary': '#3253FA', 
+			},
+		},
 	},
 	plugins: [],
 }
