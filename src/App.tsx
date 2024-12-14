f1f5c9ca0abd168eaa5c95ca4874a09de4a61e9d
quincyqqe <0972900039@mail.ru>
2024-12-15 00:50:24 +0100

    add nextui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ErrorPage from './pages/ErrorPage'
+import Header from './components/Header/Header'
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
+				<Header />
 				<Routes>
 					<Route path='/' element={<h1>Home</h1>} />
 					<Route path='*' element={<ErrorPage />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
+const { nextui } = require('@nextui-org/react')
+
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	content: [
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+	],
+
 	theme: {
 		extend: {
 			colors: {
-				'blue-primary': '#3253FA', 
+				'blue-primary': '#3253FA',
 			},
 		},
 	},
-	plugins: [],
+	darkMode: 'class',
+	plugins: [
+		nextui({
+			defaultTheme: 'dark',
+		}),
+	],
 }
