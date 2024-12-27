9373942b8f4a5e5ccc488994ddaa87b8e8bc2395
quincyqqe <0972900039@mail.ru>
2024-12-27 07:40:13 +0100

    project fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,18 +5,18 @@ import TrendingPage from '../pages/TrendingPage'
 import Layout from '../components/Layout/Layout'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../shared/api/query-client'
+import AnimeInfoPage from '../pages/AnimeInfoPage'
 
 export function App() {
-	
 	return (
 		<>
 			<QueryClientProvider client={queryClient}>
 				<BrowserRouter>
 					<Routes>
 						<Route path='/' element={<Layout />}>
-						
 							<Route path='/' element={<MainPage />} />
 							<Route path='/trending' element={<TrendingPage />} />
+							<Route path='info/:id' element={<AnimeInfoPage />} />
 						</Route>
 						<Route path='*' element={<ErrorPage />} />
 					</Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,5 @@ module.exports = {
 				},
 			},
 		}),
-		[require('tailwind-scrollbar')({ nocompatible: true })],
 	],
 }
